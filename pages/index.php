be7397d8fe51ca9223d3842c6403b516f67515b5
Yaël <larkyriver@gmail.com>
2023-02-03 19:10:14 +0100

    légère suite

--- pages/index.php
+++ pages/index.php
@@ -3,7 +3,7 @@
     <section>
         <p class="bolder section_title">About me</p>
 
-        <p>Hi, I’m Yaël, a french junior developper based in Île-de-France in the University Gustave Eiffel</p>
+        <p>Hi, I’m Yaël, a french junior developper based in Île-de-France in the University Gustave Eiffel.</p>
     </section>
 
     <section>
@@ -47,5 +47,46 @@
             </div>
         </div>
 
+    </section>
+
+    <section>
+        <p class="bolder section_title">My skills</p>
+
+        <div class="skills_container card">
+            <div class="skills_category">
+                <div class="skill_category__item">
+                    <p class="skill_self__label">All</p>
+                </div>
+                <div class="skill_category__item">
+                    <p class="skill_self__label">Software</p>
+                </div>
+                <div class="skill_category__item">
+                    <p class="skill_self__label">Front-end</p>
+                </div>
+                <div class="skill_category__item">
+                    <p class="skill_self__label">Back-end</p>
+                </div>
+                <div class="skill_category__item">
+                    <p class="skill_self__label">Design</p>
+                </div>
+                <div class="skill_category__item">
+                    <p class="skill_self__label">Editing</p>
+                </div>
+                <div class="skill_category__item">
+                    <p class="skill_self__label">Soft skills</p>
+                </div>
+                <div class="skill_category__item">
+                    <p class="skill_self__label">Other</p>
+                </div>
+            </div>
+
+            <div class="separator"></div>
+
+            <div class="skills_items">
+                <div class="skill_item">
+                    <p>PS</p>
+                </div>
+            </div>
+        </div>
     </section>
 </div>
