edf1958a1fc28c77fff9e992c0ddba652418f978
Yaël <larkyriver@gmail.com>
2023-02-10 12:25:49 +0100

    ongoing : ajout des icons de skills

--- pages/index.php
+++ pages/index.php
@@ -54,7 +54,7 @@
 
         <div class="skills_container card">
             <div class="skills_category">
-                <div class="skill_category__item">
+                <div class="skill_category__item skill_category__item--active">
                     <p class="skill_self__label">All</p>
                 </div>
                 <div class="skill_category__item">
@@ -75,18 +75,173 @@
                 <div class="skill_category__item">
                     <p class="skill_self__label">Soft skills</p>
                 </div>
-                <div class="skill_category__item">
+                <!-- <div class="skill_category__item">
                     <p class="skill_self__label">Other</p>
-                </div>
+                </div> -->
             </div>
 
             <div class="separator"></div>
 
             <div class="skills_items">
                 <div class="skill_item">
-                    <p>PS</p>
+                    <div class="skill_item-logo">
+                        <img src="/src/img/java.svg" alt="Java logo">
+                    </div>
+                    <p class="skill_item-name">Java</p>
                 </div>
+
+                <div class="skill_item">
+                    <div class="skill_item-logo">
+                        <img src="/src/img/office.svg" alt="MS Office logo">
+                    </div>
+                    <p class="skill_item-name">Office</p>
+                </div>
+
+                <div class="skill_item">
+                    <div class="skill_item-logo">
+                        <img src="/src/img/node.svg" alt="Node JS logo">
+                    </div>
+                    <p class="skill_item-name">NodeJs</p>
+                </div>
+
+                <div class="skill_item">
+                    <div class="skill_item-logo">
+                        <img src="/src/img/github.svg" alt="Github logo">
+                    </div>
+                    <p class="skill_item-name">Github</p>
+                </div>
+
+                <div class="skill_item">
+                    <div class="skill_item-logo">
+                        <img src="/src/img/python.svg" alt="Python logo">
+                    </div>
+                    <p class="skill_item-name">Python</p>
+                </div>
+
+                <div class="skill_item">
+                    <div class="skill_item-logo">
+                        <img src="/src/img/js.svg" alt="JavaScript logo">
+                    </div>
+                    <p class="skill_item-name">JavaScript</p>
+                </div>
+
+                <div class="skill_item">
+                    <div class="skill_item-logo">
+                        <img src="/src/img/vscode.svg" alt="VS Code logo">
+                    </div>
+                    <p class="skill_item-name">VS Code</p>
+                </div>
+
+                <div class="skill_item">
+                    <div class="skill_item-logo">
+                        <img src="/src/img/aftereffect.svg" alt="Adobe After Effect logo">
+                    </div>
+                    <p class="skill_item-name">After Effect</p>
+                </div>
+
+                <div class="skill_item">
+                    <div class="skill_item-logo">
+                        <img src="/src/img/photoshop.svg" alt="Adobe PhotoShop logo">
+                    </div>
+                    <p class="skill_item-name">PhotoShop</p>
+                </div>
+
+                <div class="skill_item">
+                    <div class="skill_item-logo">
+                        <img src="/src/img/premierepro.svg" alt="Adobe Premiere Pro logo">
+                    </div>
+                    <p class="skill_item-name">Premiere Pro</p>
+                </div>
+
+                <div class="skill_item">
+                    <div class="skill_item-logo">
+                        <img src="/src/img/bash.svg" alt="Bash logo">
+                    </div>
+                    <p class="skill_item-name">Bash</p>
+                </div>
+
+                <div class="skill_item">
+                    <div class="skill_item-logo">
+                        <img src="/src/img/c.svg" alt="C logo">
+                    </div>
+                    <p class="skill_item-name">C</p>
+                </div>
+
+                <div class="skill_item">
+                    <div class="skill_item-logo">
+                        <img src="/src/img/css.svg" alt="CSS logo">
+                    </div>
+                    <p class="skill_item-name">CSS</p>
+                </div>
+
+                <div class="skill_item">
+                    <div class="skill_item-logo">
+                        <img src="/src/img/figma.svg" alt="Figma logo">
+                    </div>
+                    <p class="skill_item-name">Figma</p>
+                </div>
+
+                <div class="skill_item">
+                    <div class="skill_item-logo">
+                        <img src="/src/img/git.svg" alt="Git logo">
+                    </div>
+                    <p class="skill_item-name">Git</p>
+                </div>
+
+                <div class="skill_item">
+                    <div class="skill_item-logo">
+                        <img src="/src/img/drive.svg" alt="Drive logo">
+                    </div>
+                    <p class="skill_item-name">Drive</p>
+                </div>
+
+                <div class="skill_item">
+                    <div class="skill_item-logo">
+                        <img src="/src/img/html.svg" alt="HTML logo">
+                    </div>
+                    <p class="skill_item-name">HTML</p>
+                </div>
+
+                <div class="skill_item">
+                    <div class="skill_item-logo">
+                        <img src="/src/img/intellij.svg" alt="Jetbrain's Intellij logo">
+                    </div>
+                    <p class="skill_item-name">Intellij</p>
+                </div>
+
+                <div class="skill_item">
+                    <div class="skill_item-logo">
+                        <img src="/src/img/sql.svg" alt="SQL logo">
+                    </div>
+                    <p class="skill_item-name">SQL</p>
+                </div>
+
+                <div class="skill_item">
+                    <div class="skill_item-logo">
+                        <img src="/src/img/communication.svg" alt="Communication logo">
+                    </div>
+                    <p class="skill_item-name">Communication</p>
+                </div>
+
+                <div class="skill_item">
+                    <div class="skill_item-logo">
+                        <img src="/src/img/agile.svg" alt="Agile icon">
+                    </div>
+                    <p class="skill_item-name">Agile</p>
+                </div>
+
+                <div class="skill_item">
+                    <div class="skill_item-logo">
+                        <img src="/src/img/creative.svg" alt="Creative icon">
+                    </div>
+                    <p class="skill_item-name">Creative</p>
+                </div>
+
             </div>
         </div>
+    </section>
+
+    <section>
+        <p class="bolder section_title">My career</p>
     </section>
 </div>
